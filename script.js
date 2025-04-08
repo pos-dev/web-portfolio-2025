3107877cc32694becfb9a295649f2e8ff8c4d352
Poese Toomata <toomatapoese@gmail.com>
2025-04-08 13:25:31 +0800

    readded whole site

--- script.js
+++ script.js
@@ -277,6 +277,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('globe').appendChild(renderer.domElement);
 
